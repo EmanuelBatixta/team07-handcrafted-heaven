02f936171bf64e89f9da65c4d3ca2de596e74b48
emanueljonatas <emanueljonatas28@gmail.com>
2026-01-22 20:31:29 -0300

    Co-authored-by: calizayaalex5 <calizayaalex5@users.noreply.github.com> Co-authored-by: Tyson <TysonPace777@users.noreply.github.com> Co-authored-by: ibra1390 <ibra1390@users.noreply.github.com> Co-authored-by: Jonathas Barbosa de Oliveira <Jonthss@users.noreply.github.com>

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Nav from "@/app/nav";
+
+   
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Handcrafted Heaven | Group07</title>
+      </head>
+      <body>
+      <Header/>
+      <Nav/>
+        {children}
+      <Footer/>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/nav.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function Nav(){
+    return( 
+        <nav>
+            <Link href="/"></Link>
+            <Link href="/"></Link>
+            <Link href="/"></Link>
+            <Link href="/"></Link>
+            <Link href="/">About us</Link>
+        </nav>
+    )
+}
