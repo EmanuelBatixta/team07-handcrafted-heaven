bde2dc0d61a34b685be9822b60a201ebb9c025cd
calizayaalex5 <calizayaalex5@gmail.com>
2026-01-28 02:33:56 -0300

    first steps for the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,38 @@
+import Image from "next/image"
+import Link from 'next/link'
 import styles from './components.module.css'
 
 export default function Footer() {
-    return(
+    return (
         <>
-            <h1 className={styles.footer}>footer component</h1>
+            <div className={styles.footerContainer}>
+                <nav className={styles.footerNav} >
+                    <Link href="/">Home</Link>
+                    <Link href="/product-list">Products</Link>
+                    <Link href="/about-us">About Us</Link>
+                    <Link href="/login">Login</Link>
+                    <Link href="/register">Register</Link>
+                </nav >
 
-            <span>©{new Date().getFullYear()} All rigths reserved</span>
+
+                <div className={styles.footerTitle}>
+                    <h1 className={styles.footer}>Handcrafted Heaven</h1>
+                    <Image className="logoFooter" src="/handcraftedlogo.webp" alt="logoFooter" width={100} height={100}></Image>
+                </div>
+
+                <div className={styles.studentsList}>
+                    <p>Members:</p>
+                    <ul>
+                        <li>Alex C.</li>
+                        <li>Emanuel Batista</li>
+                        <li>John Carlo D.</li>
+                        <li>John Carlo D.</li>
+                        <li>John Carlo D.</li>
+                    </ul>
+                </div>
+
+                <span className={styles.copyright}>©{new Date().getFullYear()} All rigths reserved</span>
+            </div>
         </>
     )
 }
