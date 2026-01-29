870e3f02e4eebf2f91ea5c85fb88aa601703518a
emanueljonatas <emanueljonatas28@gmail.com>
2026-01-28 21:04:59 -0300

    update nav links

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,12 +1,27 @@
+'use client'
+
 import Link from 'next/link'
 import styles from './components.module.css'
+import clsx from 'clsx'
+import { usePathname } from 'next/navigation'
 
 export default function Nav(){
+    const pathname = usePathname()
+    const links = [{ href: '/', name: 'Home'}, {href: '/product-list', name: 'Products'},{href: '/about-us', name: 'About Us'}]
+
     return( 
         <nav className={styles.nav}>
-            <Link href="/">Home</Link>
-            <Link href="/product-list">Products</Link>
-            <Link href="/about-us">About Us</Link>
+            {links.map((link)=>{
+                return (
+                    <Link href={link.href} key={link.name} className={clsx(
+                    {
+                        [styles.actualPage] : pathname === link.href,
+
+                    },
+                )}
+            >{link.name}</Link>
+                )
+            })}
         </nav>
     )
 }
