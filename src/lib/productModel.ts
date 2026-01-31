1720a920d98f21a7781a0c5f3a1d365781502650
emanueljonatas <emanueljonatas28@gmail.com>
2026-01-30 22:58:00 -0300

    add: all models in folder lib. I will transfer all to api folder using routes to test

---
+++ src/lib/productModel.ts
@@ -0,0 +1,25 @@
+'use server'
+
+import { PrismaClient } from "../../generated/prisma/client";
+import * as z from "zod"
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const productSchema = z.object({
+    userPrivate_id: z.string(),
+    category: z.string(),
+    image_link: z.string()
+})
+
+type product = z.infer<typeof productSchema>
+
+export class Product{
+    constructor(private prisma: PrismaClient) {}
+
+    async createProduct(data: product){
+        this.prisma.product.create({data})
+    }
+
+    async deleteProduct(id: string){
+        this.prisma.product.delete({where: {id: id}})
+    }
+}
