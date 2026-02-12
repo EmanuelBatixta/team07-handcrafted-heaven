204b6ed68fb3321e6b08824ce81f045e8d83d9fd
emanueljonatas <emanueljonatas28@gmail.com>
2026-02-11 21:26:30 -0300

    Implement review API endpoints: add GET, PUT, and DELETE methods for handling reviews

---
+++ src/app/api/reviews/[id]/route.ts
@@ -0,0 +1,31 @@
+import prisma from "@/app/db/db"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params
+    const id_int = parseInt(id)
+    return NextResponse.json({data: await prisma.review.findMany({where: {productId: id_int}}), status: 200})
+}
+
+export async function PUT(request: NextRequest, {params}: {params: {id:string}}) {
+    const {id} = params
+    const body = await request.json()
+    const {data} = body
+    const id_int = parseInt(id)
+    try {
+        return NextResponse.json({data: await prisma.review.update({where: {id: id_int}, data:data}), status: 200})
+    } catch {
+        return NextResponse.json({status: 204, error: 'not possible to update'})
+    }
+}
+ 
+export async function DELETE(request: NextRequest, {params}: {params: {id:string}}) {
+    const {id} = params
+    const id_int = parseInt(id)
+    try {
+        return NextResponse.json({data: await prisma.review.delete({where: {id: id_int}})})
+    } catch {
+        return NextResponse.json({code: 400, error: 'not possible to delete'})
+    }
+}
+ 
--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -1,12 +1,6 @@
 import prisma from "@/app/db/db"
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params
-    const id_int = parseInt(id)
-    return NextResponse.json({data: await prisma.review.findMany({where: {productId: id_int}}), status: 200})
-}
-  
 export async function POST(request: NextRequest) {
     const body = await request.json()
     const { data } = body
@@ -16,26 +10,3 @@ export async function POST(request: NextRequest) {
         return NextResponse.json({status: 204, error: 'not possible create'})
     }
 }
- 
-export async function PUT(request: NextRequest, {params}: {params: {id:string}}) {
-    const {id} = params
-    const body = await request.json()
-    const {data} = body
-    const id_int = parseInt(id)
-    try {
-        return NextResponse.json({data: await prisma.review.update({where: {id: id_int}, data:data}), status: 200})
-    } catch {
-        return NextResponse.json({status: 204, error: 'not possible to update'})
-    }
-}
- 
-export async function DELETE(request: NextRequest, {params}: {params: {id:string}}) {
-    const {id} = params
-    const id_int = parseInt(id)
-    try {
-        return NextResponse.json({data: await prisma.review.delete({where: {id: id_int}})})
-    } catch {
-        return NextResponse.json({code: 400, error: 'not possible to delete'})
-    }
-}
- 
